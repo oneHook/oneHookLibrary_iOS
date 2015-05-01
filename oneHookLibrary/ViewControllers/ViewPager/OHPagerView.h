090addc83b3c76eec8141d6dfffd93acd816381a
Eagle Diao@Home <eaglediao@gmail.com>
2015-04-30 23:09:06 -0400

    view pager

---
+++ oneHookLibrary/ViewControllers/ViewPager/OHPagerView.h
@@ -0,0 +1,34 @@
+//
+//  OHPagerViewController.h
+//  oneHookLibrary
+//
+//  Created by Eagle Diao on 2015-04-30.
+//  Copyright (c) 2015 oneHook inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class OHPagerView;
+
+@protocol OHPagerViewDatasource <NSObject>
+
+- (NSInteger)numberOfPagesInPager:(OHPagerView *)pagerView;
+- (UIView *)pagerView:(OHPagerView *)pagerView viewControllerAtIndex:(NSInteger)index;
+
+@end
+
+@protocol OHPagerViewDelegate <NSObject>
+
+- (void)pagerView:(OHPagerView *)pagerView viewPurgedAt:(NSInteger)index;
+- (void)pagerView:(OHPagerView *)pagerView viewAddedAt:(NSInteger)index;
+
+@end
+
+@interface OHPagerView : UIView <UIScrollViewDelegate>
+
+@property (assign, nonatomic) NSInteger selectedIndex;
+
+@property (assign, nonatomic) id<OHPagerViewDatasource> datasource;
+@property (assign, nonatomic) id<OHPagerViewDelegate> delegate;
+
+@end
---
+++ oneHookLibrary/ViewControllers/ViewPager/OHPagerView.m
@@ -0,0 +1,132 @@
+//
+//  OHPagerViewController.m
+//  oneHookLibrary
+//
+//  Created by Eagle Diao on 2015-04-30.
+//  Copyright (c) 2015 oneHook inc. All rights reserved.
+//
+
+#import "OHPagerView.h"
+
+#define PAGE_WIDTH self.bounds.size.width
+
+@interface OHPagerView () {
+    BOOL _sized;
+}
+
+@property (strong, nonatomic) NSMutableArray *views;
+@property (strong, nonatomic) UIScrollView *scrollView;
+
+@end
+
+@implementation OHPagerView
+
+- (id)init {
+    self = [super init];
+    if (self) {
+        [self commonInit];
+    }
+    return self;
+}
+
+- (void)commonInit {
+    _sized = NO;
+    self.scrollView = [[UIScrollView alloc] init];
+    self.scrollView.pagingEnabled = YES;
+    self.scrollView.delegate = self;
+
+    [self addSubview:self.scrollView];
+
+    self.backgroundColor = [UIColor whiteColor];
+
+    self.selectedIndex = 0;
+
+    [self reloadData];
+}
+
+- (void)setDatasource:(id<OHPagerViewDatasource>)datasource {
+    _datasource = datasource;
+    [self reloadData];
+}
+
+- (void)reloadData {
+    if (self.datasource == nil || !_sized) {
+        return;
+    }
+    if (self.views == nil) {
+        self.views = [[NSMutableArray alloc] init];
+    }
+    /* TODO may want to purge all existing vcs */
+    [self.views removeAllObjects];
+    NSInteger numPages = [self.datasource numberOfPagesInPager:self];
+    for (int i = 0; i < numPages; i++) {
+        [self.views addObject:[NSNull null]];
+    }
+    self.scrollView.contentOffset = CGPointMake(0, 0);
+}
+
+- (void)layoutSubviews {
+    self.scrollView.frame = self.bounds;
+    [self onSizeChanged];
+}
+
+- (void)onSizeChanged {
+    _sized = YES;
+    NSInteger numPages = [self.datasource numberOfPagesInPager:self];
+    self.scrollView.contentSize = CGSizeMake(self.bounds.size.width * numPages, 0);
+    [self reloadData];
+}
+
+- (void)invalidatePages:(NSInteger)currIndex {
+    NSInteger totalPages = [self.datasource numberOfPagesInPager:self];
+    for (int i = 0; i < totalPages; i++) {
+        if (i == currIndex - 1) {
+            if ([[self.views objectAtIndex:i] isEqual:[NSNull null]]) {
+                [self loadViewControllerAt:i];
+            }
+        } else if (i == currIndex) {
+            if ([[self.views objectAtIndex:i] isEqual:[NSNull null]]) {
+                [self loadViewControllerAt:i];
+            }
+        } else if (i == currIndex + 1) {
+            if ([[self.views objectAtIndex:i] isEqual:[NSNull null]]) {
+                [self loadViewControllerAt:i];
+            }
+        } else {
+            if (![[self.views objectAtIndex:i] isEqual:[NSNull null]]) {
+                [self purgeViewControllerAt:i];
+            }
+        }
+    }
+}
+
+- (void)purgeViewControllerAt:(NSInteger)index {
+    NSLog(@"purse view at %ld", index);
+    UIView *v = [self.views objectAtIndex:index];
+    if (![v isEqual:[NSNull null]]) {
+        [v removeFromSuperview];
+        [self.views replaceObjectAtIndex:index withObject:[NSNull null]];
+        [self.delegate pagerView:self viewPurgedAt:index];
+    }
+}
+
+- (void)loadViewControllerAt:(NSInteger)index {
+    NSLog(@"obtain view at %ld", index);
+    UIView *vc = [self.datasource pagerView:self viewControllerAtIndex:index];
+    [self.scrollView addSubview:vc];
+    vc.frame = CGRectMake(PAGE_WIDTH * index, 0, PAGE_WIDTH, self.bounds.size.height);
+    [self.views replaceObjectAtIndex:index withObject:vc];
+    [self.delegate pagerView:self viewAddedAt:index];
+}
+
+#pragma marks - UIScrollViewDelegate
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    if (!_sized) {
+        return;
+    }
+    NSInteger currIndex = roundf(scrollView.contentOffset.x / PAGE_WIDTH);
+    [self invalidatePages:currIndex];
+}
+
+@end
