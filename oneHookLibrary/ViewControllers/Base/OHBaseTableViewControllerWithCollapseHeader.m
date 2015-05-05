ddc33e95ebb6df5e83a9d0f49486eb6395caf5d8
Eagle Diao@Home <eaglediao@gmail.com>
2015-05-04 22:23:06 -0400

    tweak

--- oneHookLibrary/ViewControllers/Base/OHBaseTableViewControllerWithCollapseHeader.m
+++ oneHookLibrary/ViewControllers/Base/OHBaseTableViewControllerWithCollapseHeader.m
@@ -86,4 +86,14 @@
     self.navigationBarBackground.alpha = navigationBarAlpha;
 }
 
+#pragma marks - UITableViewDatasource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return nil;
+}
+
 @end
