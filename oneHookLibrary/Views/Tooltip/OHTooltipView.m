a296dbb10c917d087b648e9a79d5d9939631be68
Eagle Diao@Home <eaglediao@gmail.com>
2015-06-08 22:59:32 -0400

    tooltip view cont.

--- oneHookLibrary/Views/Tooltip/OHTooltipView.m
+++ oneHookLibrary/Views/Tooltip/OHTooltipView.m
@@ -18,13 +18,6 @@
 
 @implementation OHTooltipView
 
-- (id)init {
-    if((self = [super init])) {
-        [self commonInit];
-    }
-    return self;
-}
-
 - (id)initWithFrame:(CGRect)frame {
     if ((self = [super initWithFrame:frame])) {
         [self commonInit];
@@ -46,7 +39,7 @@
     
     /* Create a containing layer and set it contents with an image */
     _containerLayer = [CALayer layer];
-   
+    
     /* Create your translucent black layer and set its opacity */
     _blackLayer = [CALayer layer];
     _blackLayer.backgroundColor = [UIColor blackColor].CGColor;
@@ -56,55 +49,41 @@
     
     /* Create a mask layer with a shape layer that has a circle path */
     _maskLayer = [CAShapeLayer layer];
-    _maskLayer.borderColor = [UIColor blackColor].CGColor;
-    _maskLayer.borderWidth = 5.0f;
     _maskLayer.fillRule = kCAFillRuleEvenOdd;
-//    
-//    
-//    // Center the mask layer in the translucent black layer
-//    [maskLayer setPosition:
-//     CGPointMake([translucentBlackLayer bounds].size.width/2.0f,
-//                 [translucentBlackLayer bounds].size.height/2.0f)];
     
     [_blackLayer setMask:_maskLayer];
     [self.layer addSublayer:_containerLayer];
-    
-    _containerLayer.opacity = 0.0f;
 }
 
 - (void)layoutSubviews
 {
-    _containerLayer.bounds = self.bounds;
-    _containerLayer.position = self.center;
-    
-    _blackLayer.bounds = self.bounds;
-    _blackLayer.position = self.center;
-    
-    _maskLayer.bounds = _containerLayer.bounds;
-    _maskLayer.position = self.center;
-    
+    [super layoutSubviews];
+    self.layer.frame = self.bounds;
+    _containerLayer.frame = self.bounds;
+    _blackLayer.frame = self.bounds;
+    _maskLayer.frame = self.bounds;
 }
 
 - (void)setFocusArea:(CGRect)focusArea
 {
-//    // When you create a path, remember that origin is in upper left hand
-//    // corner, so you have to treat it as if it has an anchor point of 0.0,
-//    // 0.0
-//    UIBezierPath *path = [UIBezierPath bezierPathWithOvalInRect:
-//                          self.addChallengeButton.frame];
-//    
-//    // Append a rectangular path around the mask layer so that
-//    // we can use the even/odd fill rule to invert the mask
-//    [path appendPath:[UIBezierPath bezierPathWithRect:[maskLayer bounds]]];
-//    
-//    // Set the path's fill color since layer masks depend on alpha
-//    [maskLayer setFillColor:[[UIColor blackColor] CGColor]];
-//    [maskLayer setPath:[path CGPath]];
-
+    _focusArea = focusArea;
+    
+    // When you create a path, remember that origin is in upper left hand
+    // corner, so you have to treat it as if it has an anchor point of 0.0,
+    // 0.0
+    UIBezierPath *path = [UIBezierPath bezierPathWithOvalInRect:focusArea];
+    
+    // Append a rectangular path around the mask layer so that
+    // we can use the even/odd fill rule to invert the mask
+    [path appendPath:[UIBezierPath bezierPathWithRect:self.bounds]];
+    
+    // Set the path's fill color since layer masks depend on alpha
+    [_maskLayer setFillColor:[[UIColor blackColor] CGColor]];
+    [_maskLayer setPath:[path CGPath]];
 }
 
 - (void)onTooltipViewClicked:(UITapGestureRecognizer*)rec {
-    
+    [self animateDismiss];
 }
 
 - (void)animateReveal
@@ -112,9 +91,26 @@
     CABasicAnimation* animation = [CABasicAnimation animationWithKeyPath:@"opacity"];
     animation.fromValue = [NSNumber numberWithFloat:0.0f];
     animation.toValue = [NSNumber numberWithFloat:1.0f];
-    animation.duration = 0.5f;
+    animation.duration = 0.3f;
+    animation.removedOnCompletion = YES;
+    animation.fillMode = kCAFillModeForwards;
     [_containerLayer addAnimation:animation forKey:@"alphaanimation"];
 }
 
+- (void)animateDismiss
+{
+    [CATransaction begin];
+    CABasicAnimation* animation = [CABasicAnimation animationWithKeyPath:@"opacity"];
+    animation.fromValue = [NSNumber numberWithFloat:1.0f];
+    animation.toValue = [NSNumber numberWithFloat:0.0f];
+    animation.duration = 0.3f;
+    animation.removedOnCompletion = YES;
+    animation.fillMode = kCAFillModeForwards;
+    [CATransaction setCompletionBlock:^{
+        [self removeFromSuperview];
+    }];
+    [_containerLayer addAnimation:animation forKey:@"alphaanimation"];
+    [CATransaction commit];
+}
 
 @end
