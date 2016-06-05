460ffdee0f4d8fe11888f265777c302721051def
Eagle Diao <eaglediao@gmail.com>
2016-06-05 12:44:21 -0400

    cont.

---
+++ oneHookLibrary/Demo/ViewControllers/OHViewControllerWithToolbarDemo.h
@@ -0,0 +1,13 @@
+//
+//  OHViewControllerWithToolbarDemo.h
+//  oneHookLibrary
+//
+//  Created by Eagle Diao@ToMore on 2016-06-05.
+//  Copyright © 2016 oneHook inc. All rights reserved.
+//
+
+#import "OHViewControllerWithToolbar.h"
+
+@interface OHViewControllerWithToolbarDemo : OHViewControllerWithToolbar
+
+@end
---
+++ oneHookLibrary/Demo/ViewControllers/OHViewControllerWithToolbarDemo.m
@@ -0,0 +1,19 @@
+//
+//  OHViewControllerWithToolbarDemo.m
+//  oneHookLibrary
+//
+//  Created by Eagle Diao@ToMore on 2016-06-05.
+//  Copyright © 2016 oneHook inc. All rights reserved.
+//
+
+#import "OHViewControllerWithToolbarDemo.h"
+
+@implementation OHViewControllerWithToolbarDemo
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+@end
