146eade51ce415059c32ad8410cb268000a91037
Eagle Diao <eaglediao@gmail.com>
2016-06-05 12:37:26 -0400

    code clean up

---
+++ oneHookLibrary/Widget/Toolbar/OHToolbar.h
@@ -0,0 +1,13 @@
+//
+//  OHToolbar.h
+//  oneHookLibrary
+//
+//  Created by Eagle Diao@ToMore on 2016-06-05.
+//  Copyright © 2016 oneHook inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OHToolbar : UIView
+
+@end
---
+++ oneHookLibrary/Widget/Toolbar/OHToolbar.m
@@ -0,0 +1,13 @@
+//
+//  OHToolbar.m
+//  oneHookLibrary
+//
+//  Created by Eagle Diao@ToMore on 2016-06-05.
+//  Copyright © 2016 oneHook inc. All rights reserved.
+//
+
+#import "OHToolbar.h"
+
+@implementation OHToolbar
+
+@end
