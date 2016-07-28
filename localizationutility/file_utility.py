926a3a51cab195fba7744e96ca53d30a8fbdfa71
Eagle Diao@Home <eaglediao@gmail.com>
2016-07-28 12:47:16 -0400

    cont.

--- localizationutility/file_utility.py
+++ localizationutility/file_utility.py
@@ -2,6 +2,8 @@ import os
 import re
 
 string_re = re.compile('(?<!NSLog\()@"[^"]+"')
+
+
 ## print(string_re.search('''this is a typical line @"abfdsfc"'''))
 
 def scan_m_files(target):
@@ -15,12 +17,23 @@ def scan_m_files(target):
 
 
 def scan_lines(filename):
-    input = open(filename)
-    line = input.readline()
+    rv = []
+    finput = open(filename)
+    line = finput.readline()
     while line != "":
-        matches = string_re.search(line)
-        if matches != None:
+        start = 0
+        match = string_re.search(line, start)
+        while match != None:
+            print("Found ({0}, {1})".format(match.start(), match.end()))
             print(line)
-        line = input.readline()
-    input.close()
+            print(" " * match.start() + "^" + " " * (match.end() - match.start() - 2) + "^")
+            rv.append("{0} = {0}".format(line[match.start():match.end()]))
+            # key = input("Press enter to change: ")
+            # if key == "":
+            #     print("Change ")
+            print("-" * 15)
+            match = string_re.search(line, match.end())
+        line = finput.readline()
+    finput.close()
+    return rv
 
--- localizationutility/localization_organizer.py
+++ localizationutility/localization_organizer.py
@@ -4,8 +4,16 @@ target = "../../Dooo-iOS"
 
 mfiles = utility.scan_m_files(target)
 
+fout = open("output.txt", "w")
+
 for fp in mfiles:
     print("Scan " + fp[0])
-    utility.scan_lines(fp[0])
 
+    lines = utility.scan_lines(fp[0])
+    if len(lines) > 0:
+        fout.write("/* {0} */\n\n".format(fp[1]))
+        fout.writelines(list(map(lambda x: x + "\n", lines)))
+        fout.write("\n\n")
+
+fout.close()
 
